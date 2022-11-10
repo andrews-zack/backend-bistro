d8ae781ead70e9630af7fd7c7d356d943a233eb4
andrews-zack <zachary.andrews.au19@gmail.com>
2022-11-10 16:18:08 +0000

    returned json data

--- myproject/backend_bistro/models.py
+++ myproject/backend_bistro/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=40, null=False, unique=True)
     desc = models.CharField(max_length=200, null=False)
     price = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+    spicy = models.PositiveIntegerField(default=0, validators = [MinValueValidator(0), MaxValueValidator(5)])
     cuisine_id = models.ForeignKey("Cuisine", on_delete=models.CASCADE)
     category_id = models.ForeignKey("Category", on_delete=models.CASCADE)
 
--- myproject/backend_bistro/views.py
+++ myproject/backend_bistro/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import MenuItem, Category, Cuisine
 from pprint import pprint as pront
+from django.forms.models import model_to_dict
 
 def get_menu(request):
-    pront(dir(request))
+    menu = list(MenuItem.objects.values())
+    # for x in menu:
+    #     model_to_dict(x)
+    return JsonResponse({'data': menu})
 
